ea8fd859dd98a7e9e680a0786843937722f913f3
shopia1122 <shopia123@sookmyung.ac.kr>
2025-11-30 22:23:29 +0900

    main1

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct student {
+       int ID; //학번
+       char name[20]; //이름
+       double grade; //학점
+        
+       };
+
+int main(int argc, char *argv[])
+{
+    //변수 선언 및 초기화
+    struct student stud = {10, "Seongeun", 4.3};
+    struct student *stdPtr;
+    
+    stdPtr = &stud;
+    
+    //학번 이름 학점 값 변경
+    stdPtr->ID = 17;
+    strcpy(stdPtr->name, "AHN");
+    stdPtr->grade = 2.0; 
+    
+    //각각 출력
+    printf("ID : %i, name: %s, grade : %lf\n", 
+                stdPtr->ID, stdPtr->name, stdPtr->grade); 
+  
+  system("PAUSE");	
+  return 0;
+}
